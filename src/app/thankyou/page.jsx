fad4c39328054eeb9760524d2614d50169769f87
Vivek Viradia <vivekviradia123@gmail.com>
2023-08-02 15:30:34 +0530

    Issue Fixed

--- src/app/thankyou/page.jsx
+++ src/app/thankyou/page.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-import TotalPriceModal from "@/components/TotalPriceModel";
 
 const ThankyouPage = () => {
   const useParams = useSearchParams();
