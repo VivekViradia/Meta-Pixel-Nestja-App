9090a496d5900beecc98f832a70ca7b86f093986
Vivek Viradia <vivekviradia123@gmail.com>
2023-08-03 14:17:23 +0530

    Product Listing event gtm

--- src/app/productPage/page.jsx
+++ src/app/productPage/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import ProductListing from "@/components/ProductListing";
 import { ProductData123 } from "@/constants";
 import { usePathname } from "next/navigation";
@@ -7,7 +7,13 @@ import { usePathname } from "next/navigation";
 export const ProductPage = () => {
   const pathname = usePathname();
   console.log("pathname", pathname);
-
+  useEffect(() => {
+    window.dataLayer.push({
+      event: "Product Listing",
+      pagePath: "/productpage",
+      pageName: "Product Listing",
+    });
+  }, []);
   return <ProductListing productData={ProductData123} />;
 };
 
